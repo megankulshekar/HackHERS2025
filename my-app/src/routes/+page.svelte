a6ff434f5421e5f5a8b0ea93581240d9234ae4fa
misok267 <mekhiahc218@gmail.com>
2025-02-08 18:15:58 -0500

    attempt

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -21,13 +21,6 @@ body{
 <button type="button" class="btn btn-primary mr-xs mb-sm">home</button>
 <button type="button" class="btn btn-secondary mr-xs mb-sm">about</button>
 <button type="button" class="btn btn-tertiary mr-xs mb-sm">backend</button>
-
-
-
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h1>home</h1>
-<p>this is the home page.</p>
-
 </body>
 
 </html>
