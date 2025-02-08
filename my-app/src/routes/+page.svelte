fe34f9d4adda3eb62ce773dbbc94384694ee10c2
misok267 <mekhiahc218@gmail.com>
2025-02-08 17:52:49 -0500

    h1

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,25 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+<style>
 h1 {
-	color: plum;
-	text-align: center;
+	color:rgb(138, 92, 191);
+	text-align: center; 
 }
 
+</style>
 </head>
-<h1>Women in Tech</h1>
+</body>
 
+
+<h1>Women in Tech</h1>
 
 <nav>
 	<a href="/">home</a>
 	<a href="/about">about</a>
     <a href="/backend">backend</a>
 </nav>
-<button type="button" class="btn btn-primary mr-xs mb-sm">Primary</button>
-<button type="button" class="btn btn-secondary mr-xs mb-sm">Secondary</button>
-<button type="button" class="btn btn-tertiary mr-xs mb-sm">Tertiary</button>
-<button type="button" class="btn btn-quaternary mr-xs mb-sm">Quaternary</button>
+
+
+<button type="button" class="btn btn-primary mr-xs mb-sm">home</button>
+<button type="button" class="btn btn-secondary mr-xs mb-sm">about</button>
+<button type="button" class="btn btn-tertiary mr-xs mb-sm">backend</button>
+
 
 
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <h1>home</h1>
 <p>this is the home page.</p>
 
+
+
+</html> 
