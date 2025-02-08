a1afc7e31dd96b76026874e73f898bad2eb3e6c9
misok267 <mekhiahc218@gmail.com>
2025-02-08 16:49:39 -0500

    home

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,10 @@
+<head>
+h1 {
+	color: plum;
+	text-align: center;
+}
+
+</head>
 <h1>Women in Tech</h1>
 
 
@@ -6,11 +13,13 @@
 	<a href="/about">about</a>
     <a href="/backend">backend</a>
 </nav>
+<button type="button" class="btn btn-primary mr-xs mb-sm">Primary</button>
+<button type="button" class="btn btn-secondary mr-xs mb-sm">Secondary</button>
+<button type="button" class="btn btn-tertiary mr-xs mb-sm">Tertiary</button>
+<button type="button" class="btn btn-quaternary mr-xs mb-sm">Quaternary</button>
+
 
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <h1>home</h1>
 <p>this is the home page.</p>
 
-<button type="button" class="btn btn-primary mr-xs mb-sm">home</button>
-<button type="button" class="btn btn-secondary mr-xs mb-sm">about</button>
-<button type="button" class="btn btn-tertiary mr-xs mb-sm">backend</button>
