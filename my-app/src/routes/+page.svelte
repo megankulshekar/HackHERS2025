b46696010ed863d3f7600fb1793568d95a8d254b
revatigg <167470881+revatigg@users.noreply.github.com>
2025-02-08 15:07:29 -0500

    fixed merge

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <h2>HackHERS!!!</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>home</h1>
+<p>this is the home page.</p>
