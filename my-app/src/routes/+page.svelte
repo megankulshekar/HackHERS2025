7d44f06bd9f0d5cd2c3d57650e5b73fb6623f51b
misok267 <mekhiahc218@gmail.com>
2025-02-08 16:39:09 -0500

    home

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,40 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<h2>HackHERS!!!</h2>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Women in Tech</h1>
+
+
 <nav>
 	<a href="/">home</a>
 	<a href="/about">about</a>
     <a href="/backend">backend</a>
 </nav>
 
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <h1>home</h1>
 <p>this is the home page.</p>
 
-<button type="button" class="btn btn-primary mr-xs mb-sm">Primary</button>
-<button type="button" class="btn btn-secondary mr-xs mb-sm">Secondary</button>
-<button type="button" class="btn btn-tertiary mr-xs mb-sm">Tertiary</button>
-<button type="button" class="btn btn-quaternary mr-xs mb-sm">Quaternary</button>
-
-<div class="church">
-	<div class="thumb-info custom-thumb-info custom-box-shadow thumb-info-hide-wrapper-bg custom-sm-margin-bottom-1">
-		<span class="thumb-info-wrapper">
-			<a href="#">
-				<img alt="candles" src="/portals/22/Church/candles.jpg" class="img-fluid" />
-			</a>
-		</span>
-		<div class="thumb-info-caption">
-			<div class="custom-event-infos">
-				<ul>
-					<li><em class="fas fa-clock"></em> 5:00 PM</li>
-					<li class="text-uppercase"><em class="fas fa-map-marker-alt"></em> New York</li>
-				</ul>
-			</div>
-
-			<div class="thumb-info-caption-text">
-				<h4 class="font-weight-bold mb-sm"><a class="text-color-dark" href="#">Mauris sagitis urna molestie </a></h4>
-
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a lectus vitae arcu bibendum condimentum. In a porta nunc ...</p>
-			</div>
-		</div>
-	</div>
-</div>
+<button type="button" class="btn btn-primary mr-xs mb-sm">home</button>
+<button type="button" class="btn btn-secondary mr-xs mb-sm">about</button>
+<button type="button" class="btn btn-tertiary mr-xs mb-sm">backend</button>
