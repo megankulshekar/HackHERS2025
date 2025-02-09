98a83b187953b6d1bd0d88d4d85703d9988408c3
misok267 <mekhiahc218@gmail.com>
2025-02-09 10:11:29 -0500

    homepage update

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -42,7 +42,7 @@
       box-shadow: 0 4px 3px;
       margin-bottom: 10px;
       margin-top: 50px;
-      border-radius: 25px
+      border-radius: 20px
     }
 
     
@@ -50,28 +50,47 @@
       height: 500px; 
     }
 
-/*     
-    .button {
-      padding: 10px;
-      background-color: #007BFF;
-      color: white;
-      border: none;
-      cursor: pointer;
-      margin-top: 10px;
-      border-radius: 5px;
-    } */
-
-    
+    /*     
+  .button {
+    padding: 10px;
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    cursor: pointer;
+    margin-top: 10px;
+    border-radius: 5px;
+  } */
 </style>
 <h1>Women in Tech</h1>
 <h2>Database Connection Status:</h2>
   <p>{dbStatus}</p>
 
-<nav>
+<!-- <nav>
     <a href="/">home</a>
     <a href="/articles">articles</a>
     <a href="/signup">signup</a>
     <a href="/menteeview"> mentee view</a>
     <a href="/mentorview">mentor view</a>
     <a href="/jobs">jobs</a>
-</nav>
+</nav> -->
+
+  
+<a href="/"><button>home</button></a>
+<a href="/signup"><button>signup</button></a>
+<a href="/articles"><button>articles</button></a>
+<a href ="/menteeview"><button>mentee view</button></a>
+<a href="/mentorview"><button>mentor view</button></a>
+<a href="/jobs"><button>jobs</button></a>
+
+
+
+<div class="scroll-container" id="scrollContainer">
+  <div class="content">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p>Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+    <p>Suspendisse potenti. Sed egestas est in feugiat.</p>
+    <p>Phasellus auctor urna at est sollicitudin interdum.</p>
+    <p>Donec non justo ac purus fringilla ullamcorper at sed urna.</p>
+    <p>Curabitur mollis, ante ut venenatis sodales, magna eros sodales eros.</p>
+  </div>
+</div>
