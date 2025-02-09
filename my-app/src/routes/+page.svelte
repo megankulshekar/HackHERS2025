377bdebbb23db823d93fd0d114ee19115282152a
misok267 <mekhiahc218@gmail.com>
2025-02-09 09:53:41 -0500

    textbox

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -13,6 +13,37 @@
         color:rgb(138, 92, 191);
         text-align: center; 
     }
+    .scroll-container {
+      margin: auto;
+      text-align: center;
+      width: 650px;
+      height: 450px;
+      overflow-y: scroll; /* Enable vertical scrolling */
+      border: 1px solid #333;
+      background-color: rgb(243, 243, 243);
+      box-shadow: 0 4px 3px;
+      margin-bottom: 10px;
+      margin-top: 50px;
+      border-radius: 25px
+    }
+
+    
+    .content {
+      height: 500px; 
+    }
+
+/*     
+    .button {
+      padding: 10px;
+      background-color: #007BFF;
+      color: white;
+      border: none;
+      cursor: pointer;
+      margin-top: 10px;
+      border-radius: 5px;
+    } */
+
+    
 </style>
 
 <h1>Women in Tech</h1>
@@ -39,3 +70,16 @@
   <a href="mentorship">
     <button>mentorship</button>
   </a>
+
+  <div class="scroll-container" id="scrollContainer">
+    <div class="content">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+      <p>Suspendisse potenti. Sed egestas est in feugiat.</p>
+      <p>Phasellus auctor urna at est sollicitudin interdum.</p>
+      <p>Donec non justo ac purus fringilla ullamcorper at sed urna.</p>
+      <p>Curabitur mollis, ante ut venenatis sodales, magna eros sodales eros.</p>
+    </div>
+  </div>
+
+ 
