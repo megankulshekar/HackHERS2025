ff7c9d90ca51be9eb9d26fe4b7fe24f724fe3b25
misok267 <mekhiahc218@gmail.com>
2025-02-08 19:21:56 -0500

    attempt 3

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,14 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	
+		<style>
+			body {
+				font-family: 'Marcellus', serif;
+				background-color: rgb(208, 191, 226);
+			}
+			h1 {
+				color:rgb(138, 92, 191);
+				text-align: center; 
+			}
+		</style>
+	</head>
+	<body>
+		<nav>
+		<a href="/">home</a>
+	    <a href="/about">about</a>
+        <a href="/signup">signup</a>
+		 </nav>
+		<h1>Women in Tech</h1>
 
-<h1>Women in Tech</h1>
+		<!-- <button type="button" class="btn btn-primary mr-xs mb-sm">home</button>
+		<button type="button" class="btn btn-secondary mr-xs mb-sm">about</button>
+		<button type="button" class="btn btn-tertiary mr-xs mb-sm">backend</button> -->
 
-<nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
-    <a href="/signup">signup</a>
-</nav>
-
-
-<button type="button" class="btn btn-primary mr-xs mb-sm">home</button>
-<button type="button" class="btn btn-secondary mr-xs mb-sm">about</button>
-<button type="button" class="btn btn-tertiary mr-xs mb-sm">backend</button>
-
+	</body>
+</html>
