8efa0b2f5345e3439b42af042554f864a09c2a73
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2025-02-08 20:37:27 -0500

    restructured home page

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,29 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-		<link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Marcellus+SC&display=swap" rel="stylesheet">
-		<style>
-		
-			body {
-				font-family: 'Marcellus', serif;
-				background-color: rgb(208, 191, 226);
-			}
-			h1 {
-				color:rgb(138, 92, 191);
-				text-align: center; 
-			}
-		</style>
-	</head>
-	<body>
-		<nav>
-		<a href="/">home</a>
-	    <a href="/articles">articles</a>
-        <a href="/signup">signup</a>
-		<a href="mentorship"> mentorship</a> 
-		 </nav>
-		<h1>Women in Tech</h1>
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Marcellus&family=Marcellus+SC&display=swap" rel="stylesheet">
+</svelte:head>
 
-	</body>
-</html>
+<style>
+	:global(body) {
+        font-family: 'Marcellus', serif;
+        background-color: rgb(208, 191, 226);
+    }
+    h1 {
+        color:rgb(138, 92, 191);
+        text-align: center; 
+    }
+</style>
+
+<h1>Women in Tech</h1>
+
+<nav>
+    <a href="/">home</a>
+    <a href="/articles">articles</a>
+    <a href="/signup">signup</a>
+    <a href="mentorship"> mentorship</a>
+</nav>
